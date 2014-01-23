bed9dc97be26f676900e394fe736eb868bf083e5
erogitconfig --global user.name erogitconfigt --global user.email jonathan.popham@gmail.comgit init repocd repoecho Hi <jonathan.popham@gmail.com>
2014-01-22 19:20:57 -0500

    Pick Your Sound initial commit
    Transitioning Pickyoursound.com to a Ruby on Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pickyoursoundonrails::Application.config.secret_key_base = 'a444fdbd7ab86a410cc6647655a9eaa7ca4da9a936453fe320322850cd5334abb238bc792a5169253dd5cedc32bd50e856f013d33ffeceaa57984c59ae0e45ed'
